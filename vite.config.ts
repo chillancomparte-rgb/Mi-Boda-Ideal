3ad23a13840a9f4a9c306ad2b1dd88b6053afd55
chillancomparte-rgb <chillancomparte@gmail.com>
2025-11-02 03:08:30 -0300

    error3

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'; // Importar 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: { // Añadir configuración de resolución
+    alias: {
+      '@services': path.resolve(__dirname, './services'),
+      '@types': path.resolve(__dirname, './types'),
+    },
+  },
   server: {
     proxy: {
       '/api': {
